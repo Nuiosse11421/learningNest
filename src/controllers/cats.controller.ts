7d4a1c2c9592b365851b8518b14c3a5fda8839b3
Nuiosse11421 <notice.gameacc@gmail.com>
2024-04-27 20:37:14 +0700

    delete:controller cats

--- src/controllers/cats.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from "@nestjs/common";
-
-@Controller()
-export class CatsController {
-    @Get()
-    findAll():string{
-        return 'This action retuns all cats'
-    }
-}
