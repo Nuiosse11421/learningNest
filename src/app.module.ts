6b8f6b317fe29bc4acd873cbfe15cb6c4b12d524
Nuiosse11421 <notice.gameacc@gmail.com>
2024-05-24 17:20:45 +0700

    modified: write line to connect mongoose for use the cloud database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
 @Module({
-  imports:[CatsModule, UsersModule]
+  imports: [MongooseModule.forRoot('mongodb+srv://notice:ima1@nestdb.zl4huag.mongodb.net/learning-nest?retryWrites=true&w=majority&appName=NESTDB'),
+    CatsModule,
+    UsersModule]
 })
 export class AppModule {}
